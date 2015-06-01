9c54c331103819f406c70ccbf7cc7065b4760a17
orlein <pcj9024@gmail.com>
2015-06-01 18:16:34 +0900

    1차 완성 - 버그 및 요구사항 미이행 조사 필요

--- src/com/mzen/pcj/AddressBook/app/AddressBookMain.java
+++ src/com/mzen/pcj/AddressBook/app/AddressBookMain.java
@@ -3,6 +3,7 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserInterface ui = new UserInterface();
+		
 		Thread th = new Thread(ui);
 		th.start();
 	}
